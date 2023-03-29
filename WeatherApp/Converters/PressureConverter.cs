327e6877e0888d520b671bce8de857c3a66e74c1
Vitalii Voitovych <vitalikvv2119@gmail.com>
2023-03-29 16:10:43 +0300

    Add PressureConverter. Some changes UI

---
+++ WeatherApp/Converters/PressureConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace WeatherApp.Converters;
+
+public class PressureConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var pressure = (double)value;
+        return pressure * 0.7501;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
